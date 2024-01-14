76c80e426c4ea255fc18938d39014f475592c757
Xiangyu Ji <57505625+angyuMathmo@users.noreply.github.com>
2024-01-15 04:04:16 +0800

    Updated py files

---
+++ fasta_to_txt.py
@@ -0,0 +1,58 @@
+from Bio import SeqIO
+
+def read_fasta_file(file_path):
+    """
+    Reads a FASTA file and returns a list of sequences.
+    """
+    sequences = []
+
+    with open(file_path, "r") as fasta_file:
+        for record in SeqIO.parse(fasta_file, "fasta"):
+
+            sequences.append(record)
+
+    return sequences
+
+# Specify the path to your FASTA file
+fasta_file_path = "/Users/seanji02/Documents/HKRA/IntNet/raw/TFILE1.fasta"
+
+# Read the FASTA file
+fasta_sequences = read_fasta_file(fasta_file_path)
+
+# Print the sequences
+data = []
+for sequence in fasta_sequences:
+    data.append(sequence.id)
+data_clean = list(range(len(data)))
+
+print(data[5000][:3]=='NC_' or 'NZ_')
+data_clean[5000]=data[5000].split("_")[0]+ data[5000].split("_")[1]
+print(data_clean[5000])
+print(data[20330].split("|"))
+print(data[20330][:10]=='Smyshlyaev' or data[20330][:5]=='Zhang' or len(data[20330].split("|"))==2 or len(data[20330].split("|"))==3 )
+
+
+# Data Clean with selection rule specified
+for i in range(len(data)):
+    
+    if data[i][:3]=='NC_' or  data[i][:3]=='NZ_':
+        data_clean[i]=data[i].split("_")[0]+ data[i].split("_")[1] # Starts with NZ/NC: require format "NC_015945.1"
+    if data[i][:3]=='GCF':
+        data_clean[i]=data[i].split("_")[2]+ data[i].split("_")[3]  # Starts with GCF: require convert "GCF_020309905.1_NZ_CP084323.1_4621" to "NZ_CP084323.1"
+    if data[i][:3]=='gnl':
+        data_clean[i]=data[i].split("|")[1] # Starts with gnl: require convert "gnl|AB257723|idbi84_uncultured_bacterium|intI|p100|q100|s100" to "AB257723"
+    if data[i][:10]=='Smyshlyaev' or data[i][:5]=='Zhang' or len(data[i].split("|"))==2 or len(data[i].split("|"))==3:
+        data_clean[i]='0'   # The rest : to be solved
+
+for i in range(len(data)):
+    if data_clean[i]==i:   # For the rest, all of them have a clear accession number, simply fetch them
+        data_clean[i]=data[i].split("_")[:1][0]
+
+print(data_clean)
+
+# Open the file in write mode
+output_file_path = "output2.txt"
+with open(output_file_path, 'w') as output_file:
+    # Write each line of data to the file
+    for line in data_clean:
+        output_file.write(str(line) + '\n')
--- grabNCBI_protein.py
+++ grabNCBI_protein.py
@@ -1,40 +1,101 @@
 from Bio import Entrez, SeqIO
+import pandas as pd
+import json 
 
-def fetch_protein_info(protein_name):
+
+import fasta_to_txt
+gene_id_list=fasta_to_txt.data_clean
+
+def fetch_protein_info(gene_id):
     Entrez.email = "xiangyu270208@gmail.com"  # Set your email for NCBI API usage
 
-    # Search for the protein of interest in NCBI
-    handle = Entrez.esearch(db="protein", term=protein_name)
-    record = Entrez.read(handle)
+    # Retrieve the gene record
+    handle = Entrez.efetch(db="nucleotide", id=gene_id, rettype="gb", retmode="text")
+    gene_record = SeqIO.read(handle, "genbank")
     handle.close()
+    
 
-    if len(record["IdList"]) == 0:
-        print(f"No records found for protein: {protein_name}")
-        return
+    list_of_titles=[]
+    for i in range(len(gene_record.annotations["references"])):
+        list_of_titles.append(gene_record.annotations["references"][i].title)
+    
 
-    # Retrieve the first protein record
-    protein_id = record["IdList"][0]
-    handle = Entrez.efetch(db="protein", id=protein_id, rettype="gb", retmode="text")
-    protein_record = SeqIO.read(handle, "genbank")
-    handle.close()
+    if 'organism' not in list(gene_record.features[0].qualifiers.keys()):
+        gene_record.features[0].qualifiers['organism'] = '0'
+    if 'isolation_source' not in list(gene_record.features[0].qualifiers.keys()):
+        gene_record.features[0].qualifiers['isolation_source'] = '0'
+    if 'collection_date' not in list(gene_record.features[0].qualifiers.keys()):
+        gene_record.features[0].qualifiers['collection_date'] = '0'
+    if 'country' not in list(gene_record.features[0].qualifiers.keys()):
+        gene_record.features[0].qualifiers['country'] = '0'
+    if 'db_xref' not in list(gene_record.features[0].qualifiers.keys()):
+        gene_record.features[0].qualifiers['db_xref'] = '0'
 
-    # Print the information
-    print(dir(protein_record))
-    print("LOCUS       {}".format(protein_record.id))
-    print("DEFINITION  {}".format(protein_record.description))
-    print("ACCESSION   {}".format(protein_record.id))
-    print("DBLINK      {}".format(protein_record.annotations.get("dblink", "")))
-    print("SOURCE      {}".format(protein_record.annotations.get("source", "")))
 
+    data=[gene_record.id,gene_record.description,gene_record.annotations["source"],
+        gene_record.features[0].qualifiers["organism"][0],gene_record.features[0].qualifiers["isolation_source"][0],
+        gene_record.features[0].qualifiers["collection_date"][0],gene_record.features[0].qualifiers["country"][0],
+        gene_record.features[0].qualifiers["db_xref"][0]]
+    
+
+    for i in range(len(gene_record.dbxrefs)):
+        data.append(gene_record.dbxrefs[i])
+    for j in range(len(list_of_titles)):
+        data.append(list_of_titles[j])
+
+
+    
+    ## Print the information
+    #print("NCBI Accession Number  {}".format(gene_record.id))
+    #print("Definition             {}".format(gene_record.description))
+    #print("DBLink                 {}".format(gene_record.dbxrefs))
+    #print("Source                 {}".format(gene_record.annotations["source"]))
+    #print("References             {}".format(list_of_titles))
+    #print("Organism               {}".format(gene_record.features[0].qualifiers["organism"][0]))
+    #print("Isolation Source       {}".format(gene_record.features[0].qualifiers["isolation_source"][0]))
+    #print("Collection Date        {}".format(gene_record.features[0].qualifiers["collection_date"][0]))
+    #print("Country                {}".format(gene_record.features[0].qualifiers["country"][0]))
+    #print("Taxon ID               {}".format(gene_record.features[0].qualifiers["db_xref"][0]))
+    
+ 
+    
+    return data
+    
     # Additional information can be printed based on your requirements
     # For example, you can print features, references, etc.
+         
 
-    # Print the sequence
-    print("ORIGIN")
-    sequence = str(protein_record.seq)
-    for i in range(0, len(sequence), 60):
-        print(" " + sequence[i:i + 60])
+# Example usage with the provided gene id
+data_file="stored_data.json"
+for i in range(3372,len(gene_id_list)):
+  print(i)
+  gene_id = gene_id_list[i]
+  print(gene_id)
+  data = fetch_protein_info(gene_id)
 
-# Example usage with the provided protein name
-protein_name = "integron [Burkholderia pseudomallei]"
-fetch_protein_info(protein_name)
+  try:
+        with open(data_file, 'r') as file:
+            existing_data = json.load(file)
+  except FileNotFoundError:
+        existing_data = []
+  
+  existing_data.append(data)
+
+  with open(data_file, 'w') as file:
+        json.dump(existing_data, file)
+
+with open(data_file, 'r') as file:
+    ds = json.load(file) 
+
+df = pd.DataFrame(ds,columns=['NCBI Accession Number','Definition','Source','Organism', 'Isolation Source', 'Coolection Date', 'Country', 'Taxon ID', 'BioProject', 'Biosample', 'Ref'  ])
+
+
+#note: in some truncations, might be some repeated items, need to clean 
+#note: i=1814 failed
+
+
+#def unique(lists):
+#    unique_list = pd.Series(lists).drop_duplicates().tolist()
+#    for x in unique_list:
+#        print(x)
+#unique(lists)
